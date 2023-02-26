1c2f9b8e9b3da6682288c89756febdc54e93067f
Masoud <78568883+masoodhesami@users.noreply.github.com>
2023-02-27 00:14:56 +0330

    code seprate to components

---
+++ app/restaurant/[slug]/components/ResMenu.tsx
@@ -0,0 +1,15 @@
+import ResMenuCard from "./ResMenuCard";
+
+export default function ResMenu() {
+    return (
+        <main className="bg-white mt-5">
+            <div>
+                <div className="mt-4 pb-1 mb-1">
+                    <h1 className="font-bold text-4xl">Menu</h1>
+                </div>
+                <div className="flex flex-wrap justify-between">
+                    <ResMenuCard />
+                </div>
+            </div>
+        </main>)
+}
