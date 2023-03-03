41df3a4e522759b6cc56e963b229689acee2ab63
Masoud <78568883+masoodhesami@users.noreply.github.com>
2023-03-03 17:16:41 +0330

    make restaurant page dynamic

--- app/restaurant/[slug]/layout.tsx
+++ app/restaurant/[slug]/layout.tsx
@@ -2,12 +2,15 @@ import ResHeader from "./components/ResHeader";
 
 export default function Restaurantlayout({
     children,
+    params
 }: {
-    children: React.ReactNode
+    children: React.ReactNode;
+    params: { slug: string }
 }) {
+
     return (
         <>
-            <ResHeader />
+            <ResHeader name={params.slug} />
             <div className="flex m-auto w-2/3 justify-between items-start 0 -mt-11 text-black">
                 {children}
             </div>
--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,5 @@
+import { PrismaClient } from "@prisma/client";
 import ResDescription from "./components/ResDescription";
-import ResHeader from "./components/ResHeader";
 import ResImages from "./components/ResImages";
 import ResNavbar from "./components/ResNavbar";
 import ResRating from "./components/ResRating";
@@ -7,20 +7,51 @@ import ResReservationCard from "./components/ResReservationCard";
 import ResReviews from "./components/ResReviews";
 import ResTitle from "./components/ResTitle";
 
-export default function RestaurantDetails() {
+const prisma = new PrismaClient()
+
+interface Restaurant {
+    id: number;
+    name: string;
+    images: string[];
+    description: string;
+    slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: {
+            slug
+        },
+        select: {
+            id: true,
+            name: true,
+            images: true,
+            description: true,
+            slug: true
+        }
+    })
+    if (!restaurant) {
+        throw new Error()
+    }
+    return restaurant;
+}
+
+export default async function RestaurantDetails({ params }: { params: { slug: string } }) {
+
+    const restaurant = await fetchRestaurantBySlug(params.slug)
     return (
         <>
-                <div className="bg-white w-[70%] rounded p-3 shadow">
-                    <ResNavbar />
-                    <ResTitle />
-                    <ResRating />
-                    <ResDescription />
-                    <ResImages />
-                    <ResReviews />
-                </div>
-                <div className="w-[27%] relative text-reg">
-                    <ResReservationCard />
-                </div>
+            <div className="bg-white w-[70%] rounded p-3 shadow">
+                <ResNavbar slug={restaurant.slug} />
+                <ResTitle name={restaurant.name} />
+                <ResRating />
+                <ResDescription description ={restaurant.description} />
+                <ResImages images={restaurant.images}/>
+                <ResReviews />
+            </div>
+            <div className="w-[27%] relative text-reg">
+                <ResReservationCard />
+            </div>
         </>
     )
 }
