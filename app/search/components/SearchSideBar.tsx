e8c38469af05bebf725bb926c29def3ef987d785
Masoud <78568883+masoodhesami@users.noreply.github.com>
2023-03-05 00:28:24 +0330

    search query set and render cards with specefic location

--- app/search/components/SearchSideBar.tsx
+++ app/search/components/SearchSideBar.tsx
@@ -1,6 +1,6 @@
 export default function SearchSideBar() {
     return (
-        <div className="w-1/5">
+        <div className="w-1/5 m-4">
             <div className="border-b pb-4">
                 <h1 className="mb-2">Region</h1>
                 <p className="font-light text-reg">Toronto</p>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,51 @@
+import { PrismaClient } from "@prisma/client";
 import SearchHeader from "./components/SearchHeader";
 import SearchRestaurantCard from "./components/SearchRestaurantCard";
 import SearchSideBar from "./components/SearchSideBar";
 
-export default function Search() {
+const prisma = new PrismaClient();
+
+const fetchResturantsByLocation = async (name: string | undefined) => {
+    const location = await prisma.location.findMany({
+        where: {
+            name
+        }
+    });
+
+    if (location.length !== 0) {
+        const restaurants = await prisma.restaurant.findMany({
+            where: {
+                location_id: location[0]?.id
+            },
+            select: {
+                name: true,
+                main_image: true,
+                price: true,
+                slug: true,
+            }
+        });
+        return restaurants;
+    }
+}
+
+
+export default async function Search({
+    searchParams,
+}: {
+    searchParams?: { [key: string]: string | string[] | undefined };
+}) {
+
+    const searchLocation = searchParams?.city?.toString().toLowerCase();
+    const restaurants = await fetchResturantsByLocation(searchLocation);
     return (
         <>
             <SearchHeader />
             <div className="flex py-4 m-auto w-2/3 justify-between items-start text-black">
                 <SearchSideBar />
                 <div className="w-5/6">
-                    <SearchRestaurantCard />
+                    {!restaurants ? <h2 className="text-2xl">There is no restaurants in this city !</h2> : restaurants.map(restaurant => (
+                        <SearchRestaurantCard restaurant={restaurant} />
+                    ))}
                 </div>
             </div>
         </>
